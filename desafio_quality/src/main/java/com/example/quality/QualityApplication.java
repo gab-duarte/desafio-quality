09954037a1d6fda8f4e448cb91dfc3da3c22c58a
gab-duarte <gabriel.duarte@mercadolivre.com>
2021-06-10 12:11:17 -0300

    setup project

---
+++ desafio_quality/src/main/java/com/example/quality/QualityApplication.java
@@ -0,0 +1,13 @@
+package com.example.quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityApplication.class, args);
+	}
+
+}
---
+++ desafio_quality/src/test/java/com/example/quality/QualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
